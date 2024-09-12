1bd464ecc359b683dbb227f34f485d6f2de1fbb7
GJC <599953528@qq.com>
2024-09-12 17:31:22 +0800

    组合

--- src/main/java/com/github/hcsp/inheritance/Sub.java
+++ src/main/java/com/github/hcsp/inheritance/Sub.java
@@ -1,15 +1,8 @@
 package com.github.hcsp.inheritance;
 
-public class Sub  {
-    private final Base base;
-
-    public Sub(Base base) {
-        this.base = base;
-    }
+public class Sub extends Base {
+    @Override
     public void sayHello() {
         System.out.println("我是子类！");
-    }
-    public void delegateSayHello() {
-        base.sayHello();
     }
 }
